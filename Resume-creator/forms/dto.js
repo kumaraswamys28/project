f1407fd22e6e71a8ec0efbcb068a1837473b93ec
KUMARASWAMY S <87463693+Manojkumbi@users.noreply.github.com>
2025-03-07 12:30:52 +0530

    Update dto.js

--- Resume-creator/forms/dto.js
+++ Resume-creator/forms/dto.js
@@ -6,9 +6,17 @@ const project = JSON.parse(localStorage.getItem("projects")) || [];
 
 
 
-document.getElementById("test").innerHTML =project==[] && education==[] && certifications==[] && personnel=={} && skillsData=={} ? `
-<button onclick="printDiv()">Print</button>
-`;
+document.getElementById("test").innerHTML =project==[] && education==[] && certifications==[] && personnel=={} && skillsData=={} ? `<div id="test">
+    <ul id="no-data-message">
+        <li><span class="emoji">📜</span> Your resume is currently a blank scroll of wisdom...</li>
+        <li><span class="emoji">🚀</span> Great things start with zero, but this is a bit too empty!</li>
+        <li><span class="emoji">📝</span> Please provide some details so we can craft your masterpiece.</li>
+        <li><span class="emoji">💡</span> Even Shakespeare had to start with words!</li>
+    </ul>
+    <button onclick="printDiv()">Print A4 Div</button>
+</div>:<div id="test">
+    <button onclick="printDiv()">Print A4 Div</button>
+</div>`;
 
 
 const printarea=document.getElementById("printArea")
